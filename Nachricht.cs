13c8c513d71b9444fc21e89a45eebc175b0b09d0
Thorsten Meyer <meyer.t@hhek.bonn.de>
2020-12-22 14:54:55 +0100

    Projektdateien hinzufügen.

---
+++ Nachricht.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTestSender
+{
+    class Nachricht
+    {
+        private string wert1;
+        private string wert2;
+
+        public string Wert1 { get => wert1; set => wert1 = value; }
+        public string Wert2 { get => wert2; set => wert2 = value; }
+
+        public Nachricht()
+        {
+            Wert1 = "Hallo";
+            Wert2 = "Freunde";
+        }
+
+        public void init(string value1,string value2)
+        {
+            Wert1 = value1;
+            Wert2 = value2;
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTestSender
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            Nachricht testobjekt = new Nachricht();
+
+            //Achtung Propertys der Klasse Nachricht müssen public sein!
+            string json1 = JsonConvert.SerializeObject(testobjekt);            
+
+            string url = "http://localhost:5000/api/Nachrichten";
+
+            //senden
+            Task<HttpResponseMessage> response = client.PostAsJsonAsync(url, json1);
+
+            try
+            {
+                response.Wait();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            //Antwort auslesen
+            HttpResponseMessage result = response.Result;
+
+            Task<string> content = result.Content.ReadAsStringAsync();
+
+            try
+            {
+                content.Wait();
+            }
+            catch(Exception)
+            {
+
+            }
+
+            string empfang = content.Result;
+
+            List<string> zurueck = JsonConvert.DeserializeObject<List<string>>(empfang);
+        }
+    }
+}
